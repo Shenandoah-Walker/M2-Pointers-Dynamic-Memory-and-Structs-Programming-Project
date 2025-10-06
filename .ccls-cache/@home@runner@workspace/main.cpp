33922c7af2e1fa728309aa4d3e95a10ecfe57a90
Shenandoah-Walker <saw67402@email.vccs.edu>
2025-10-06 15:40:24 +0000

    Worked on Course Grade Program

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,63 +1,162 @@
-//Reverse Array Program
+//Course Grade Program
+
 
 #include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+
 
 using namespace std;
+//Struct Definition
+struct student {
+  string name;
+  int id;
+  double* testScores;
+  double average;
+  char letterGrade;
+};
+
 /*
-Function: ReverseArray
-Purpose: To reverse the contents of an array
+Function: getData
+Purpose: To read the data from a file and store it in a struct
 Parameters:
- - int const arr[]: The array to be reversed
- - int size: The size of the array
-Returns: A pointer to the reversed array
-Preconditions: The array must be populated with values
-Postconditions: The array is reversed
+ - file: the file to read from
+ - studentCnt: the number of students
+ - testsCnt: the number of tests
+Returns: a pointer to the array of structs
+Preconditions: the file has been successfully opened
+Postconditions: the data has been read and stored in the struct
 */
 
-int *ReverseArray(int const arr[], int size);
+student* getData(ifstream& inputFile, int& studentCnt, int& testsCnt);
 
-/* 
-Function: print
-Purpose: To print the contents of an array
+/*
+Function: calcAverage
+Purpose: To calculate the average of the test grades and the course grade for each student
 Parameters:
- - int const arr[]: The array to be printed
- - int size: The size of the array
+ - students[]: the array of structs
+ - studentCnt: the number of students
+ - testsCnt: the number of tests
 Returns: None
-Preconditions: The array must be populated with values
-Postconditions: The array is printed to the console
+Preconditions: the data has been read and stored in the struct
+Postconditions: the average and course grade have been calculated and stored in the struct
 */
 
-void print(int const arr[], int size);
+void calcAverage(student students[], int studentCnt, int testsCnt);
 
-int main() { 
-    //Create an array of integers that will be reversed. The array size and the contents may be changed if desired.
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+/*
+Function: createReport
+Purpose: To print a formatted report
+Parameters:
+ - students[]: the array of structs
+ - studentCnt: the number of students
+ - testsCnt: the number of tests
+Returns: None
+Preconditions: the data has been read and stored in the struct
+Postconditions: a formatted report has been printed
+*/
 
-    //Call the ReverseArray function and store the returned pointer in a pointer variable.
-    int *reversed = ReverseArray(arr, 10);
-    //Use the print function to print the original array and the reversed array.
-    cout << "Original array:" << endl;
-    print(arr, 10);
-    cout << endl;
-    cout << "Reversed array:" << endl;
-    print(reversed, 10);
+void createReport(student students[], int studentCnt, int testsCnt);
 
-   //Delete the reversed array and end the program.
-    delete[] reversed;
-    return 0;
+/*
+Function: calcLetterGrade
+Purpose: Calculates a letter grade from an average test score. This function is provided
+with the test score and returns the letter grade.
+Parameters:
+ - average: The average test score
+Returns: The letter grade as a char
+Preconditions: The average must be a valid double.
+Postconditions: The letter grade will be returned as a char.
+*/
+
+char calcLetterGrade(double average);
+
+
+
+int main() {
+    //Create the variables that hold the number of students and the number of tests
+    int studentCnt, testsCnt;
+    //Create the array of structs
+    student* students;
+    //Create the variables needed to read the data from the file
+    ifstream inputFile;
+    string fileName;
+
+    /*Prompt the user to input the name of the file that contains the list of students, their ID numbers, and their test scores. Open the file and confirm that it was opened successfully. If the file was not opened successfully, end the program.*/
+    cout << "Please enter the name of the file that contains the list of students, their ID numbers, and their test scores: ";
+    cin >> fileName;
+    inputFile.open(fileName);
+    if (inputFile.fail()) {
+        cout << "Error opening file. Please restart the program and enter a valid file name." << endl;
+        return 1;  
+    }
+
+    //Call the getData function to read the data from the file and store it in the struct
+    students = getData(inputFile, studentCnt, testsCnt);
+    //Call the calcAverage function to calculate the average of the test grades and the course grade for each student
+    calcAverage(students, studentCnt, testsCnt);
+    //Call the createReport function to print a formatted report
+    createReport(students, studentCnt, testsCnt);
+    //Close the file
+    inputFile.close();
+    //Delete the array of structs
+    delete students;
+    //End the program
+    return 0;  
 }
 
-int *ReverseArray(int const arr[], int size) {
-    int *reversed = new int[size];
-    for (int i = 0; i < size; i++) {
-        reversed[i] = arr[size - i - 1];
+student* getData(ifstream& inputFile, int& studentCnt, int& testsCnt) {
+    inputFile >> studentCnt >> testsCnt;
+    student* students = new student[studentCnt];
+    for (int i = 0; i < studentCnt; i++) {
+        inputFile >> students[i].name >> students[i].id;
+        students[i].testScores = new double[testsCnt];
+        for (int j = 0; j < testsCnt; j++) {
+            inputFile >> students[i].testScores[j];
+            }
     }
-    return reversed;
+    return students;
+    
 }
 
-void print(int const arr[], int size) {
-    for (int i = 0; i < size; i++) {
-        cout << arr[i] << " ";
+void calcAverage(student students[], int studentCnt, int testsCnt) {
+    for (int i = 0; i < studentCnt; i++) {
+        students[i].average = 0;
+        
+        for (int j = 0; j < testsCnt; j++) {
+            students[i].average += students[i].testScores[j];
+            }
+        students[i].average /= testsCnt;
+        students[i].letterGrade = calcLetterGrade(students[i].average);
+        delete[] students[i].testScores;
     }
+}
+
+void createReport(student students[], int studentCnt, int testsCnt) {
+    cout << fixed << setprecision(1);
     cout << endl;
+    cout << "Student Name" << setw(25) << "Student ID" << setw(23) << "Average Test Score" << setw(25) << "Letter Grade" << endl;
+    cout << "-------------------------------------------------------------------------" << endl;
+    for (int count = 0; count < studentCnt; count++)
+        cout << students[count].name << setw(25) << students[count].id << setw(25) << students[count].average << setw(25) << students[count].letterGrade << endl;
+}
+
+
+char calcLetterGrade(double average) {
+  if (average >= 90) {
+      return 'A';
+  }
+  else if (average >= 80) {
+      return 'B';
+  }
+  else if (average >= 70) {
+      return 'C';
+  }
+  else if (average >= 60) {
+      return 'D';
+  }
+  else {
+      return 'F';
+  }
 }
--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 //Course Grade Program
 
+
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <fstream>
+
+
+using namespace std;
 //Struct Definition
 struct student {
   string name;
   int id;
+  double* testScores;
   double average;
   char letterGrade;
 };
@@ -20,7 +29,7 @@ Preconditions: the file has been successfully opened
 Postconditions: the data has been read and stored in the struct
 */
 
-student* getData(ifstream& file, int& studentCnt, int& testsCnt);
+student* getData(ifstream& inputFile, int& studentCnt, int& testsCnt);
 
 /*
 Function: calcAverage
@@ -64,15 +73,74 @@ Postconditions: The letter grade will be returned as a char.
 char calcLetterGrade(double average);
 
 
-#include <iostream>
-
-using namespace std;
 
 int main() {
+    //Create the variables that hold the number of students and the number of tests
+    int studentCnt, testsCnt;
+    //Create the array of structs
+    student* students;
+    //Create the variables needed to read the data from the file
+    ifstream inputFile;
+    string fileName;
 
-  
+    /*Prompt the user to input the name of the file that contains the list of students, their ID numbers, and their test scores. Open the file and confirm that it was opened successfully. If the file was not opened successfully, end the program.*/
+    cout << "Please enter the name of the file that contains the list of students, their ID numbers, and their test scores: ";
+    cin >> fileName;
+    inputFile.open(fileName);
+    if (inputFile.fail()) {
+        cout << "Error opening file. Please restart the program and enter a valid file name." << endl;
+        return 1;  
+    }
+
+    //Call the getData function to read the data from the file and store it in the struct
+    students = getData(inputFile, studentCnt, testsCnt);
+    //Call the calcAverage function to calculate the average of the test grades and the course grade for each student
+    calcAverage(students, studentCnt, testsCnt);
+    //Call the createReport function to print a formatted report
+    createReport(students, studentCnt, testsCnt);
+    //Close the file
+    inputFile.close();
+    //Delete the array of structs
+    delete students;
+    //End the program
+    return 0;  
 }
 
+student* getData(ifstream& inputFile, int& studentCnt, int& testsCnt) {
+    inputFile >> studentCnt >> testsCnt;
+    student* students = new student[studentCnt];
+    for (int i = 0; i < studentCnt; i++) {
+        inputFile >> students[i].name >> students[i].id;
+        students[i].testScores = new double[testsCnt];
+        for (int j = 0; j < testsCnt; j++) {
+            inputFile >> students[i].testScores[j];
+            }
+    }
+    return students;
+    
+}
+
+void calcAverage(student students[], int studentCnt, int testsCnt) {
+    for (int i = 0; i < studentCnt; i++) {
+        students[i].average = 0;
+        
+        for (int j = 0; j < testsCnt; j++) {
+            students[i].average += students[i].testScores[j];
+            }
+        students[i].average /= testsCnt;
+        students[i].letterGrade = calcLetterGrade(students[i].average);
+        delete[] students[i].testScores;
+    }
+}
+
+void createReport(student students[], int studentCnt, int testsCnt) {
+    cout << fixed << setprecision(1);
+    cout << endl;
+    cout << "Student Name" << setw(25) << "Student ID" << setw(23) << "Average Test Score" << setw(25) << "Letter Grade" << endl;
+    cout << "-------------------------------------------------------------------------" << endl;
+    for (int count = 0; count < studentCnt; count++)
+        cout << students[count].name << setw(25) << students[count].id << setw(25) << students[count].average << setw(25) << students[count].letterGrade << endl;
+}
 
 
 char calcLetterGrade(double average) {
